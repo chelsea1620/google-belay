a0d20aa28a6bb68e2197e0939501730aef1b7e52
Mark Lentczner <mzero@google.com>
2011-06-23 10:26:37 -0700

    support all CORS requests by responding to OPTIONS

---
+++ buzzer/buzzer.js
@@ -0,0 +1,68 @@
+var me = os.topDiv;
+
+os.ui.resize(150, 200, true);
+
+var formAjax = function(form, callback) {
+  var data = {};
+  for (var i = 0; i < form.elements.length; ++i) {
+    var input = form.elements[i];
+    if (input.type == 'text' || input.type == 'textarea') {
+      data[input.name] = input.value;
+    }
+    else if (input.type == 'submit') {
+      // do nothing
+    }
+    else {
+      os.alert('Unhandled type of form input: ' +
+               input.type + ' named ' + input.name);
+    }
+  }
+  os.jQuery.ajax({
+    url: form.action,
+    type: form.method || 'GET',
+    data: data,
+    dataType: 'json',
+    error: function(xhr, status, err) {
+      os.alert('form update failed: ' + status);
+    },
+    success: function(data, status, xhr) { callback(data); }
+  });
+};
+
+var rcPost = 'urn:x-belay://resouce-class/social-feed/post';
+
+var capReviver = function(resClass) {
+  if (resClass == rcPost) {
+    var poster = function(data) {
+      os.jQuery.ajax({
+        url: app.caps.post,
+        type: 'POST',
+        data: {
+          body: '' + data.body,
+          via: '' + data.via
+        },
+        success: function() { reload(); }
+      });
+    };
+    return os.capServer.buildFunc(poster);
+  }
+  return null;
+};
+
+os.capServer.setReviver(capReviver);
+
+var reload = function() {
+  me.load(app.caps.editor, function() {
+    var forms = me.find('.buzzer-thing form');
+    me.find('.buzzer-thing form').submit(function(ev) {
+      formAjax(ev.target, reload);
+      ev.preventDefault();
+      return false;
+    });
+    os.ui.capDraggable(me.find('.buzzer-chit'), rcPost, function(selectedRC) {
+      return os.capServer.grantKey(selectedRC);
+    });
+  });
+};
+
+reload();
---
+++ station/station.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+
+import datetime
+import logging
+import os
+import sys
+import uuid
+from django.utils import simplejson as json
+
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+server_url = "http://" + os.environ['HTTP_HOST']
+  # TODO(mzero): this should be safer
+
+def xhr_response(response):
+  response.headers.add_header("Access-Control-Allow-Origin", "*")
+
+def xhr_content(content, content_type, response):
+  xhr_response(response)
+  response.out.write(content)
+  response.headers.add_header("Cache-Control", "no-cache")
+  response.headers.add_header("Content-Type", content_type)
+  response.headers.add_header("Expires", "Fri, 01 Jan 1990 00:00:00 GMT")
+  
+
+class StationData(db.Model):
+  pass
+
+class InstanceData(db.Model):
+  data = db.TextProperty()
+
+
+
+class GenerateHandler(webapp.RequestHandler):
+  def get(self):
+    feed_uuid = uuid.uuid4()
+    feed_id = str(feed_uuid)
+    content = server_url + "/?s=" + feed_id
+    xhr_content(content, "text/plain", self.response)
+
+
+class BaseHandler(webapp.RequestHandler):
+  class InvalidStation(Exception):
+    pass
+  class InvalidInstance(Exception):
+    pass
+
+  def validate_station(self):
+    try:
+      station_uuid = uuid.UUID(self.request.GET['s'])
+      station_id = str(station_uuid)
+      station = StationData.get_by_key_name(station_id)
+      if station == None:
+        station = StationData(key_name=station_id)
+      return station
+    except:
+      raise BaseHandler.InvalidStation()
+
+  def validate_instance(self):
+    try:
+      station = self.validate_station()
+      instance_uuid = uuid.UUID(self.request.GET['i'])
+      instance_id = str(instance_uuid)
+      instance = InstanceData.get_by_key_name(instance_id, parent=station)
+      if instance == None:
+        instance = InstanceData(key_name=instance_id, parent=station)
+      return instance
+    except:
+      raise BaseHandler.InvalidInstance()
+
+  def options(self):
+    m = self.request.headers["Access-Control-Request-Method"]
+    h = self.request.headers["Access-Control-Request-Headers"]
+
+    self.response.headers["Access-Control-Allow-Origin"] = "*"
+    self.response.headers["Access-Control-Max-Age"] = "2592000"
+    self.response.headers["Access-Control-Allow-Methods"] = m      
+    if h:
+      self.response.headers["Access-Control-Allow-Headers"] = h
+      
+          
+  def handle_exception(self, exc, debug_mode):
+    if isinstance(exc,BaseHandler.InvalidStation):
+      logging.getLogger().warn("unrecognized station")
+      self.error(404)
+    elif isinstance(exc,BaseHandler.InvalidInstance):
+      logging.getLogger().warn("unrecognized instance")
+      self.error(404)
+    else:
+      super(BaseHandler, self).handle_exception(exc, debug_mode)
+    
+
+class LaunchHandler(BaseHandler):
+  def get(self):
+    station = self.validate_station()
+    if not station.is_saved():
+      station.put()
+
+    app = {
+	  'caps': {
+	    'instances': "%s/instances?s=%s" % (server_url, station.key().name()),
+	    'instanceBase': '%s/instance?s=%s&i=' % (server_url, station.key().name())
+	  }
+	}
+
+    template = """
+    var $ = os.jQuery;
+
+    var app = %(app)s;
+
+    $.ajax({
+      url: "%(server_url)s/station.js",
+      dataType: "text",
+      success: function(data, status, xhr) {
+        cajaVM.compileModule(data)({os: os, app: app});
+      },
+      error: function(xhr, status, error) {
+        alert("Failed to load station: " + status);
+      }
+    });
+    """
+
+    # would be simpler to do this with JSON, but then have to include Django
+    # to get to the json serializer...
+
+    content = template % {
+      'app': json.dumps(app),
+      'server_url': server_url,
+    }
+
+    xhr_content(content, "text/plain", self.response)
+
+
+class InstanceHandler(BaseHandler):
+  def get(self):
+    instance = self.validate_instance()
+    xhr_content(instance.data, "text/plain;charset=UTF-8", self.response)
+      
+  def post(self):
+    instance = self.validate_instance()
+    instance.data = db.Text(self.request.body, 'UTF-8')
+    instance.put()
+    xhr_response(self.response)
+  
+  def delete(self):
+    instance = self.validate_instance()
+    instance.delete()
+    xhr_response(self.response)
+
+
+class InstancesHandler(BaseHandler):
+  def get(self):
+    station = self.validate_station()
+    
+    q = InstanceData.all(keys_only=True)
+    q.ancestor(station)
+    ids = []
+    for instanceKey in q:
+      template ='%(server_url)s/instance?s=%(station_id)s&i=%(instance_id)s'
+      instance_cap = template  % {
+          'server_url': server_url,
+          'station_id': station.key().name(),
+          'instance_id': instanceKey.name(),
+        }
+      ids.append(instance_cap)
+    
+    xhr_content(json.dumps(ids), "text/plain;charset=UTF-8", self.response)
+
+
+application = webapp.WSGIApplication(
+  [('/',        LaunchHandler),
+  ('/generate', GenerateHandler),
+  ('/instance', InstanceHandler),
+  ('/instances',InstancesHandler),
+  ],
+  debug=True)
+
+def main():
+  logging.getLogger().setLevel(logging.DEBUG)
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()

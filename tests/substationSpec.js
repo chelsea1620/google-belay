f0f4d0ea816d26b14d78b1a3154916f72e276b4d
Joe Politz <jpolitz@google.com>
2011-06-28 11:06:47 -0700

    A test for the nested-foop problem with messagechannels.  Run WebspecRunner by visiting it in a web server

---
+++ tests/substationSpec.js
@@ -0,0 +1,34 @@
+describe('SubStation protocol', function() {
+  var tunnel;
+
+  beforeEach(function() {
+    var ready;
+    var remotePort = windowManager.open('testSubstation.html',
+                                        'test_substation');
+    tunnel = new CapTunnel(remotePort);
+    waitsFor(function() { return remotePort.ready(); },
+             'port readiness timeout', 500);
+  });
+
+  afterEach(function() {
+    runs(function() { windowManager.closeAll(); });
+  });
+
+  it('should outpost from the opening side, then respond to invoke', function()
+  {
+    runs(function() {
+      var server = new CapServer();
+      tunnel.setLocalResolver(function(instID) { return server.publicInterface; });
+      var otherSideGotResponse = false;
+      var cap = server.grant(function(v) {
+        if (v === 'Got response') {
+          otherSideGotResponse = true;
+        }
+        return 'invoked';
+      });
+      tunnel.initializeAsOutpost(server, [cap]);
+      waitsFor(function() { return otherSideGotResponse; },
+               'response timeout', 250);
+    });
+  });
+});

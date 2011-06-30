6a831345925f9be638826e0be6980418680c8e29
Joe Politz <jpolitz@google.com>
2011-06-30 11:43:21 -0700

    move xhr_* methods into BcapHandler and change signatures

--- lib/belay.py
+++ lib/belay.py
@@ -23,29 +23,28 @@ class BelayException(Exception):
   pass
   
 
-def xhr_response(response):
-  response.headers.add_header("Access-Control-Allow-Origin", "*")
-
-
-def xhr_content(content, content_type, response):
-  xhr_response(response)
-  response.out.write(content)
-  response.headers.add_header("Cache-Control", "no-cache")
-  response.headers.add_header("Content-Type", content_type)
-  response.headers.add_header("Expires", "Fri, 01 Jan 1990 00:00:00 GMT")
-
 
 class BcapHandler(webapp.RequestHandler):  
   
+  def xhr_response(self):
+    self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+
+  def xhr_content(self, content, content_type):
+    self.xhr_response()
+    self.response.out.write(content)
+    self.response.headers.add_header("Cache-Control", "no-cache")
+    self.response.headers.add_header("Content-Type", content_type)
+    self.response.headers.add_header("Expires", "Fri, 01 Jan 1990 00:00:00 GMT")
+
   def bcapRequest(self):
       return json.loads(self.request.body)['value']
       
   def bcapResponse(self, jsonResp):
     resp = json.dumps({ 'value': jsonResp })
-    xhr_content(resp, "text/plain;charset=UTF-8", self.response)
+    self.xhr_content(resp, "text/plain;charset=UTF-8")
 
   def bcapNullResponse(self):
-    xhr_response(self.response)
+    self.xhr_response()
     
   # allows cross-domain requests  
   def options(self):

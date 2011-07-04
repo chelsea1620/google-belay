dce0018c6c5917b90061c7dbfb5244d713440477
Mark Lentczner <mzero@google.com>
2011-07-04 08:40:57 -0700

    your card form images   - drag and drop to upload   - store in data store as a db.Blob property   - display on load

--- bfriendr/bfriendr.js
+++ bfriendr/bfriendr.js
@@ -3,7 +3,8 @@ var $ = os.jQuery;
 var initialize = function() {
   os.ui.resize('18em', '24em', true);
 
-  var myCardDiv = os.topDiv.find('.bfriendr-card');
+  var myCardDiv = os.topDiv.find('div.bfriendr-card');
+  var myCardImageDiv = myCardDiv.find('div.bfriendr-cardimg');
   
   for (var k in app.caps) {
     app.caps[k] = os.capServer.restore(app.caps[k]);
@@ -13,6 +14,40 @@ var initialize = function() {
     myCardDiv.find('input[name=name]').val(cardInfo.name);
     myCardDiv.find('input[name=email]').val(cardInfo.email);
     myCardDiv.find('textarea').val(cardInfo.notes);
+    if (cardInfo.image) {
+      myCardImageDiv.append('<img>');
+      myCardImageDiv.find('img').attr('src', cardInfo.image);
+    }
+    
+    var imageTypeRE = /image\/.*/;
+    var preventDf = function(e) {
+        e.originalEvent.preventDefault();
+        return false;   
+    };
+    myCardImageDiv.bind('dragenter', preventDf);
+    myCardImageDiv.bind('dragover', preventDf);
+    myCardImageDiv.bind('drop', function(e) {
+      var draggedFile;
+      var dt = e.originalEvent.dataTransfer;
+      for (var i = 0; i < dt.files.length; ++i) {
+        var file = dt.files[i];
+        if (!file.type.match(imageTypeRE)) continue;
+        draggedFile = file;
+        break;
+      }
+      if (draggedFile) {
+        var fd = new os.FormData();
+        fd.append('imageFile', draggedFile);
+        $.ajax({
+          url: cardInfo.uploadImage,
+          cache: false,
+          type: 'POST',
+          contentType: false,
+          processData: false,
+          data: fd,
+        });       
+      }
+    });
   });
   
   myCardDiv.find('button').click(function() {

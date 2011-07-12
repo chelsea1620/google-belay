31d6b135bbf16708638613b8dcf990f98f99df96
Joe Politz <jpolitz@google.com>
2011-07-12 09:59:24 -0700

    Got an accept and offer across a thing

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -11,16 +11,69 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-var tabMap = Object.create(null);
+
+// { tabID : { callbackName : [ rcList, info ] }}
+var offerMap = Object.create(null);
+var acceptMap = Object.create(null);
 
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
+    var tabID = sender.tab.id;
 
+    // Just for testing
     if(request.args[0] === 'ping') { 
       chrome.tabs.sendRequest(sender.tab.id, { args: ['pong'], callbackName: request.callbackName },
                               function() { });
+      return;
     }
 
-        
+    if (request.method === 'refresh') {
+      offerMap[tabID] = Object.create(null);
+      acceptMap[tabID] = Object.create(null);
+    }
+    else if (request.method === 'offer') {
+      if (typeof request.callbackName === 'string') {
+        offerMap[tabID][request.callbackName] = request.args;
+      }
+    }
+    else if (request.method === 'accept') {
+      if (typeof request.callbackName === 'string') {
+        acceptMap[tabID][request.callbackName] = request.args;
+      }
+    }
+    else {
+      console.log('unexpected message ', request);
+    }
+
   });
 
+
+function __autoTest() {
+setInterval(function() {
+  for(var i in offerMap) {
+    for(var callback in offerMap[i]) {
+
+      var args = offerMap[i][callback];
+
+      if(! args[0][0].match(/test/)) continue;
+
+      for(var j in acceptMap) {
+        for(var callback2 in acceptMap[j]) {
+          var args2 = acceptMap[j][callback2]; 
+
+          if(! args[0][0].match(/test/)) continue;
+          
+          chrome.tabs.sendRequest(Number(i), { args: args2, callbackName: callback },
+            function(v) { 
+              var argsWithCap = args.concat([v]);
+              chrome.tabs.sendRequest(Number(j), { args: argsWithCap,
+                                                   callbackName: callback2 });
+            });
+        }
+      }
+    }
+  }
+}, 200);
+}
+
+__autoTest();

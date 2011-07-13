26b09b6275ee8d2387b1ecc399c9aa7c04972847
Mark Lentczner <mzero@google.com>
2011-07-13 15:08:55 -0700

    don't let jQuery process JSON for us

--- belay/caps.js
+++ belay/caps.js
@@ -73,6 +73,7 @@ var CAP_EXPORTS = (function() {
 
   var makeAsyncAJAX = function(url, method, data, success, failure) {
     jQuery.ajax({ data: data,
+                  dataType: 'text',
                   type: method,
                   url: url,
                   success: function(data, status, xhr) {

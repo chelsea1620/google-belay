28b9a3ebcdbcf678be739376e8bf74ccba56a7ab
arjunguha <arjunguha@gmail.com>
2011-08-30 08:35:26 -0700

    Visit localhost:9000/pick-station.html to launch a station.
    - Various miscellaneous bugfixes.
    - caps.js is needed in shared-worker.js, which cannot load jQuery because it
      uses the DOM. So, using XHR directly now.

--- belay/shared-worker.js
+++ belay/shared-worker.js
@@ -1,12 +1,16 @@
 importScripts('lib/js/caps.js');
 
-
 var station = undefined;
 
 var workerInstID = newUUIDv4();
 var workerServer = new CapServer(workerInstID);
 
 var instToTunnel = Object.create(null);
+
+function log(msg) {
+  // Tunnel intercepts "log" messages
+  loggingPort.postMessage({ op: "log", msg: msg });
+}
 
 function resolver(instID) {
   if (instID === workerInstID) { return workerServer.publicInterface; }
@@ -16,7 +20,8 @@ function resolver(instID) {
 
 workerServer.setResolver(resolver);
 
-var stationGenerator = workerServer.restore("http://localhost:9001/generate");
+var stationGenerator =
+  workerServer.restore("http://localhost:9001/belay/generate");
 
 var pendingLaunches = Object.create(null);
 
@@ -48,9 +53,12 @@ self.addEventListener('connect', function(e) {
   var port = e.ports[0];
   var iframeTunnel = new CapTunnel(port);
 
+  // Logging hack, prints to console of most recent window.
+  loggingPort = port;
+
   iframeTunnel.setLocalResolver(resolver);
   iframeTunnel.setOutpostHandler(function(outpost) {
-    outpost = workerServer.dataPostProcess(outpost);
+    outpost = workerServer.dataPostProcess(outpost);    
     instToTunnel[outpost.iframeInstID] = iframeTunnel;
     if (location.hash in pendingLaunches) {
       // client is an instance we are expecting
@@ -62,33 +70,40 @@ self.addEventListener('connect', function(e) {
         // add a cap for launching from the station, closing over outpost.windowOpen
       }
       instToTunnel[pending.instID] = iframeTunnel;
-      outpost.setUpClient.post(pending);
+      outpost.setUpClient.post({ outpost: pending });
     }
     else {
       // client might want to become an instance or the station
       outpost.setUpClient.post({
-        becomeInstance: workerServer.grant(function(launchCap) {
-          stationCaps.newInstHandler.post({
-            launchData: launchCap, // TODO(mzero): name?
-            relaunch: workerServer.grant(buildLauncher(outpost.windowLocation))
-          }) 
-        }),
-        becomeStation: workerServer.grant(function() {
-          var openerCap = launch(outpost.windowLocation);
-          outpost.localStorage.get(function(sto) {
-            if (sto.stationLaunchCap) {
-              launchStation(sto.stationLaunchCap, openerCap);
-            }
-            else {
-              stationGenerator.get(function(launchCap) {
-                sto.stationLaunchCap = launchCap;
-                outpost.localStorage.put(sto, function() {
-                  launchStation(sto.stationLaunchCap, openerCap);
+        workerInstID: workerInstID,
+        // The outpost field is forwarded unmolested to the client. Other
+        // fields are not forwarded and thus may contain caps that are
+        // exclusively for the embedded <iframe>.
+        outpost: {
+          becomeInstance: workerServer.grant(function(launchCap) {
+            stationCaps.newInstHandler.post({
+              launchData: launchCap, // TODO(mzero): name?
+              relaunch: workerServer
+                          .grant(buildLauncher(outpost.windowLocation))
+            }) 
+          }),
+          becomeStation: workerServer.grant(function() {
+            var openerCap = outpost.windowLocation;
+            outpost.localStorage.get(function(sto) {
+              if (sto.stationLaunchCap) {
+                launchStation(sto.stationLaunchCap, openerCap);
+              }
+              else {
+                stationGenerator.get(function(launchCap) {
+                  sto.stationLaunchCap = launchCap;
+                  outpost.localStorage.put(sto, function() {
+                    launchStation(sto.stationLaunchCap, openerCap);
+                  });
                 });
-              });
-            }
-          });
-        })
+              }
+            });
+          })
+        }
       })
     }
   }); // end setOutpustHandler

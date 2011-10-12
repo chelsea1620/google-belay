4152f39c8d60a360528a194b2a06e8ef5e694521
Iain McGinniss <iainmcgin@google.com>
2011-10-12 16:32:27 -0700

    clean up emote code - remove gadget related handlers, moved the generate handler to the new consistent url.

--- emote/index.js
+++ emote/index.js
@@ -1,18 +1,24 @@
-$(function() {
-  $(document.body).find('#new-emote').click(function(evt) {
-    console.log('clicked');
-    $.ajax({
-      url: '/belay/generate-instance',
-      dataType: 'text',
-      success: function(data, status, xhr) {
-        belayPort.postMessage({
-          type: 'instanceRequest',
-          gen: data
-        });
-      },
-      failure: function() {
-        alert('Failed to generate instance');
-      }
-    });
+// Copyright 2011 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+onBelayReady(function() {
+  $('#new-emote').click(function() {
+    $.post('/generate')
+      .done(function(response) {
+          var data = capServer.dataPostProcess(response);
+          belay.outpost.becomeInstance.put(data);
+      })
+      .fail(function() { alert('failed to generate :-('); });
   });
 });

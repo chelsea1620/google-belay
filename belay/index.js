c7c28171171a95ede1c1a14a0b45306d5788b659
Mark Lentczner <mzero@google.com>
2011-10-17 16:12:17 -0700

    reduce flashing when station opens

---
+++ belay/index.js
@@ -0,0 +1,110 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* local storage for belay-belay is as follows:
+  localStorage['belay'] <a processed json blob>
+    .stationLaunchCap -- cap to station to launch
+*/
+
+function visible(n) { return n.css('display') != 'none'; };
+function enable(n) { n.removeAttr('disabled'); }
+function disable(n) { n.attr('disabled', 'disabled'); }
+function setEnabled(n, state) { state ? enable(n) : disable(n) }
+
+function setUpLaunchButton(elem, params) {
+  var startId = newUUIDv4();
+  elem.attr('href', 'redirect.html');
+  elem.attr('target', startId);
+  belay.outpost.setStationStartId.put({ id: startId, params: params });
+}
+
+onBelayReady(function() {
+  var belayData = capServer.dataPostProcess(localStorage['belay']) || { };
+  var hasStation;
+  var stationCapString;
+
+  setUpLaunchButton($('#open-button a'), { version: 'new' });
+  setUpLaunchButton($('#create-button a'), { version: 'new' });
+
+  $('#create-button a').click(function() {
+    belayData.stationGenerateCap =
+      capServer.restore($('#advanced .gen:eq(0) input').val());
+    localStorage['belay'] = capServer.dataPreProcess(belayData);
+    // now let the click go on
+  });
+
+  function resetUI() {
+    hasStation = belayData && 'stationLaunchCap' in belayData && belayData.stationLaunchCap != null;
+    stationCapString = hasStation ? belayData.stationLaunchCap.serialize() : '';
+
+    if (hasStation) {
+      $('#open-button').show();
+      $('#create-button').hide();
+    } else {
+      $('#open-button').hide();
+      $('#create-button').show();
+    }
+
+    $('#station-cap').val(stationCapString);
+    disable($('#station-set'));
+    setEnabled($('#station-clear'), hasStation);
+  }
+
+  function setLaunchCap(cap) {
+    belayData.stationLaunchCap = cap;
+    localStorage['belay'] = capServer.dataPreProcess(belayData);
+    resetUI();
+  }
+
+  $('#advanced h2').click(function() { 
+    if(visible($('#advanced .content'))) {
+      $('#advanced .control').text('▸');
+      $('#advanced .content').slideUp();
+    } else {
+      $('#advanced .control').text('▾');
+      $('#advanced .content').slideDown();
+    }
+  });
+  
+  $('#station-clear').click(function() { setLaunchCap(null); });
+  $('#station-set').click(function() { 
+    var newVal = $('#station-cap').val().trim();
+    var cap = newVal != '' ? capServer.restore(newVal) : null;
+    setLaunchCap(cap);
+  });
+  
+  
+  $('#station-cap').bind('change keypress keyup keydown click', function () {
+    setEnabled($('#station-set'), $('#station-cap').val() != stationCapString);
+  });
+  
+  $('#advanced .gen').each(function() {
+    var input = $(this).find('input');
+    $(this).find('button').click(function() {
+      var gen = capServer.restore(input.val());
+      gen.get(
+        function(newLaunch) { setLaunchCap(newLaunch); },
+        function(err) { alert("Generation failed."); });
+    });
+  });
+
+  window.addEventListener('storage', function(ev) {
+    if(ev.storageArea === localStorage && ev.key == 'belay') {
+      belayData = capServer.dataPostProcess(localStorage['belay']);
+      resetUI();
+    }
+  });
+  
+  resetUI();
+})
---
+++ emote/emote.py
@@ -0,0 +1,88 @@
+# Copyright 2011 Google Inc. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/env python
+
+import datetime
+import logging
+import os
+import sys
+import uuid
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from lib.py import belay
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from django.template.loader import render_to_string
+
+
+class EmoteData(db.Model):
+  postCap = db.TextProperty()
+  nameCap = db.TextProperty()
+
+
+class GenerateHandler(belay.BcapHandler):
+  def post(self):
+    bzrInfo = self.bcapRequest()
+    emote = EmoteData()
+    emote.postCap = bzrInfo['postCap'].serialize()
+    emote.nameCap = bzrInfo['nameCap'].serialize()
+    emote.put()
+    self.bcapResponse({
+      'launch': belay.regrant(LaunchHandler, emote),
+      'icon': belay.server_url("/tool-emote.png"), 
+      'name': 'Emote to ' + bzrInfo['name']
+    })
+
+
+class LaunchHandler(belay.CapHandler):
+  def get(self):
+    self.bcapResponse({
+        'page': {
+          'html': belay.server_url("/"),
+          'window': { 'width': 300, 'height': 250 }
+        },
+        'gadget': {
+          'html': belay.server_url("/view/gadget"),
+          'scripts': [ belay.server_url("/emote.js") ]
+        },
+        'info': { 
+          'post': self.get_entity().postCap,
+          'name': self.get_entity().nameCap,
+        }
+      })
+
+
+application = webapp.WSGIApplication(
+  [(r'/cap/.*', belay.ProxyHandler),
+  ('/generate', GenerateHandler),
+  ],
+  debug=True)
+  
+
+belay.set_handlers(
+  '/cap',
+  [ ('/belay/launch', LaunchHandler),
+  ])
+
+
+def main():
+  logging.getLogger().setLevel(logging.DEBUG)
+  run_wsgi_app(application)
+
+
+if __name__ == "__main__":
+  main()

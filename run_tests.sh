1993cb98cb5b4dd386375e2b367c36eee4e2a13e
Iain McGinniss <iainmcgin@google.com>
2011-10-28 14:27:48 -0700

    more selenium tests for buzzer. run_tests.sh script will also now wait to ensure that the appengine instances are fully initialised before running the tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+echo "--- (Re)starting belay app-engine instances"
+
 ./run.sh restart
+
+echo "--- Giving appengine time to start"
+
+ready=0
+while [ `curl -sL -w '%{http_code}' 'http://localhost:9000' -o /dev/null` != 200 ]
+do 
+	echo '.\c'
+   	sleep 1s
+done
 
 if `which -s py.test`
 then py.test tests/test_all.py
--- tests/functional/belay_buzzer_tests.py
+++ tests/functional/belay_buzzer_tests.py
@@ -5,6 +5,7 @@ from page_models import *
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from belay_test_utils import *
+from selenium.webdriver.common.action_chains import *
 import time
 
 class BelayBuzzerTests(BelayTest):
@@ -18,10 +19,86 @@ class BelayBuzzerTests(BelayTest):
 
     def test_generate_instance(self):
         instance = self.bzr.create_new_instance("Testing")
-        self.assertEqual("Testing", instance.name)
+        self.assertEqual("Testing", instance.get_name())
         self.st.focus()
         self.assertFalse(self.st.is_empty())
         self.assertEqual("Testing", self.st.uncategorized().instances()[0].name())
+    
+    def test_post(self):
+        instance = self.bzr.create_new_instance("Testing")
+        instance.post("1")
+        instance.post("2")
+        instance.post("3")
+
+        posts = [post.get_content() for post in instance.get_posts()]
+        self.assertEqual(["3", "2", "1"], posts)
+
+    def test_multiple_instances(self):
+        a = self.bzr.create_new_instance("A")
+        
+        self.open_new_window("http://localhost:9004")
+        b_start = BuzzerLandingPage(self.driver)
+        b = b_start.create_new_instance("B")
+
+        a.focus()
+        a.post("i am a")
+
+        b.focus()
+        b.post("i am b")
+
+        a.focus()
+        self.assertEqual("i am a", a.get_last_post().get_content())
+        
+        b.focus()
+        self.assertEqual("i am b", b.get_last_post().get_content())
+
+        self.st.focus()
+        self.assertTrue(len(self.st.find_instances_by_name("A")) == 1)
+        self.assertTrue(len(self.st.find_instances_by_name("B")) == 1)
+    
+    def test_attribute_exchange(self):
+        st = self.st
+        instance = self.bzr.create_new_instance("My Blog")
+        st.focus()
+
+        st.personal().set_attributes({
+            "Name": "Betsy Claypool",
+            "Location": "Pennsylvania"
+        })
+
+        drag_source = self.st.uncategorized().instances()[0].get_drag_source()
+        drop_target = self.st.personal().get_drop_target()
+        ac = ActionChains(self.driver)
+        ac.drag_and_drop(drag_source, drop_target)
+        ac.perform()
+        self.wait_for(lambda drv: self.st.personal().instances() > 0)
+
+        moved = self.st.personal().instances()[0]
+        self.assertTrue("My Blog", moved.name())
+
+        instance.focus()
+        instance.get_poster_name_attribute()
+        instance.get_poster_location_attribute()
+
+
+    def test_relaunch_from_station(self):
+        instance = self.bzr.create_new_instance("Testing")
+        instance.post("test post")
+        instance.close()
+        self.st.focus()
+
+        # the station does not realise a page has closed for at least a few
+        # seconds with the current implementation, so we must wait before
+        # we attempt to reopen the site
+        time.sleep(4)
+        self.st.find_instances_by_name("Testing")[0].open(self.driver)
+        reopenedInstance = BuzzerInstancePage(self.driver)
+        self.assertEquals("Testing", reopenedInstance.get_name())
+        self.assertEquals("test post", reopenedInstance.get_last_post().get_content())
+
+    
+
+
 
 if __name__ == "__main__":
     unittest.main()

a06c34d9e7b1b9c7b9354c404ccab68fb37d485b
Jasvir Nagra <jasvir@google.com>
2011-11-10 16:16:04 -0800

    Staging change that make belay station load it's dependent modules   * sets up option to optimize before uploading to appspot   * runs as before locally   * minor fixes to doc and makefile

--- belay/index.js
+++ belay/index.js
@@ -19,122 +19,131 @@
 
 "use strict";
 
-function visible(n) { return n.css('display') != 'none'; };
-function enable(n) { n.removeAttr('disabled'); }
-function disable(n) { n.attr('disabled', 'disabled'); }
-function setEnabled(n, state) { state ? enable(n) : disable(n) }
-
-function setUpLaunchButton(elem, action) {
-  var startId = newUUIDv4();
-  elem.attr('href', 'redirect.html');
-  elem.attr('target', startId);
-  belay.outpost.expectPage.post(
-    { startId: startId, ready: capServer.grant(action) });
-}
-
-onBelayReady(function() {
-  var belayData = capServer.dataPostProcess(localStorage['belay']) || { };
-  var hasStation;
-  var stationCapString;
-
-  function openStation(activate) {
-    belayData.stationLaunchCap.post({ version: 'new' },
-      function(launchDescriptor) {
-        var instanceId = newUUIDv4();
-        
-        activate.post({
-          instanceId: instanceId,
-          isStation: true,
-          pageUrl: launchDescriptor.pageUrl || launchDescriptor.page.html,
-          outpostData: {
-            info: launchDescriptor.info,
+// TODO(jasvir): These should be modules not scripts
+require([
+    "lib/js/include-belay.js", 
+    "lib/js/caps.js", 
+    "lib/js/common.js",
+    "lib/js/belay-client.js"], 
+  function () {
+  function visible(n) { return n.css('display') != 'none'; };
+  function enable(n) { n.removeAttr('disabled'); }
+  function disable(n) { n.attr('disabled', 'disabled'); }
+  function setEnabled(n, state) { state ? enable(n) : disable(n) }
+  
+  function setUpLaunchButton(elem, action) {
+    var startId = newUUIDv4();
+    elem.attr('href', 'redirect.html');
+    elem.attr('target', startId);
+    belay.outpost.expectPage.post(
+      { startId: startId, ready: capServer.grant(action) });
+  }
+  
+  onBelayReady(function() {
+    var belayData = capServer.dataPostProcess(localStorage['belay']) || { };
+    var hasStation;
+    var stationCapString;
+  
+    function openStation(activate) {
+      belayData.stationLaunchCap.post({ version: 'new' },
+        function(launchDescriptor) {
+          var instanceId = newUUIDv4();
+          
+          activate.post({
             instanceId: instanceId,
-          }
-        });
-      },
-      function(err) { alert("Your station isn't on-line."); });
-  }
-
-  function createAndOpenStation(activate) {
-    var genCap = capServer.restore($('#advanced .gen:eq(0) input').val());
-    genCap.get(function(launchCap){
-      setLaunchCap(launchCap);
-      openStation(activate);
-    })
-  }
-
-  setUpLaunchButton($('#open-button a'), openStation);
-  setUpLaunchButton($('#create-button a'), createAndOpenStation);
-
-  $('#open-button a').click(function() {
-    // if the user clicks the launch button, we need to reinitialise
-    // it so that it can be used again, as the belay infrastructure
-    // will forget the startId allocated after the first use.
+            isStation: true,
+            pageUrl: launchDescriptor.pageUrl || launchDescriptor.page.html,
+            outpostData: {
+              info: launchDescriptor.info,
+              instanceId: instanceId,
+            }
+          });
+        },
+        function(err) { alert("Your station isn't on-line."); });
+    }
+  
+    function createAndOpenStation(activate) {
+      var genCap = capServer.restore($('#advanced .gen:eq(0) input').val());
+      genCap.get(function(launchCap){
+        setLaunchCap(launchCap);
+        openStation(activate);
+      })
+    }
+  
     setUpLaunchButton($('#open-button a'), openStation);
-  });
-
-  function resetUI() {
-    hasStation = belayData && 'stationLaunchCap' in belayData && belayData.stationLaunchCap != null;
-    stationCapString = hasStation ? belayData.stationLaunchCap.serialize() : '';
-
-    if (hasStation) {
-      $('#open-button').show();
-      $('#create-button').hide();
-    } else {
-      $('#open-button').hide();
-      $('#create-button').show();
-    }
-
-    $('#station-cap').val(stationCapString);
-    disable($('#station-set'));
-    setEnabled($('#station-clear'), hasStation);
-  }
-
-  function setLaunchCap(cap) {
-    belayData.stationLaunchCap = cap;
-    localStorage['belay'] = capServer.dataPreProcess(belayData);
-    resetUI();
-  }
-
-  $('#advanced h2').click(function() { 
-    if(visible($('#advanced .content'))) {
-      $('#advanced .control').text('▸');
-      $('#advanced .content').slideUp();
-    } else {
-      $('#advanced .control').text('▾');
-      $('#advanced .content').slideDown();
-    }
-  });
+    setUpLaunchButton($('#create-button a'), createAndOpenStation);
   
-  $('#station-clear').click(function() { setLaunchCap(null); });
-  $('#station-set').click(function() { 
-    var newVal = $('#station-cap').val().trim();
-    var cap = newVal != '' ? capServer.restore(newVal) : null;
-    setLaunchCap(cap);
-  });
-  
-  
-  $('#station-cap').bind('change keypress keyup keydown click', function () {
-    setEnabled($('#station-set'), $('#station-cap').val() != stationCapString);
-  });
-  
-  $('#advanced .gen').each(function() {
-    var input = $(this).find('input');
-    $(this).find('button').click(function() {
-      var gen = capServer.restore(input.val());
-      gen.get(
-        function(newLaunch) { setLaunchCap(newLaunch); },
-        function(err) { alert("Generation failed."); });
+    $('#open-button a').click(function() {
+      // if the user clicks the launch button, we need to reinitialise
+      // it so that it can be used again, as the belay infrastructure
+      // will forget the startId allocated after the first use.
+      setUpLaunchButton($('#open-button a'), openStation);
     });
-  });
-
-  window.addEventListener('storage', function(ev) {
-    if(ev.storageArea === localStorage && ev.key == 'belay') {
-      belayData = capServer.dataPostProcess(localStorage['belay']);
+  
+    function resetUI() {
+      hasStation = belayData && 'stationLaunchCap' in belayData && belayData.stationLaunchCap != null;
+      stationCapString = hasStation ? belayData.stationLaunchCap.serialize() : '';
+  
+      if (hasStation) {
+        $('#open-button').show();
+        $('#create-button').hide();
+      } else {
+        $('#open-button').hide();
+        $('#create-button').show();
+      }
+  
+      $('#station-cap').val(stationCapString);
+      disable($('#station-set'));
+      setEnabled($('#station-clear'), hasStation);
+    }
+  
+    function setLaunchCap(cap) {
+      belayData.stationLaunchCap = cap;
+      localStorage['belay'] = capServer.dataPreProcess(belayData);
       resetUI();
     }
-  });
   
-  resetUI();
-  window.belaytest.ready = true;
-})
+    $('#advanced h2').click(function() { 
+      if(visible($('#advanced .content'))) {
+        $('#advanced .control').text('▸');
+        $('#advanced .content').slideUp();
+      } else {
+        $('#advanced .control').text('▾');
+        $('#advanced .content').slideDown();
+      }
+    });
+    
+    $('#station-clear').click(function() { setLaunchCap(null); });
+    $('#station-set').click(function() { 
+      var newVal = $('#station-cap').val().trim();
+      var cap = newVal != '' ? capServer.restore(newVal) : null;
+      setLaunchCap(cap);
+    });
+    
+    
+    $('#station-cap').bind('change keypress keyup keydown click', function () {
+      setEnabled($('#station-set'), $('#station-cap').val() != stationCapString);
+    });
+    
+    $('#advanced .gen').each(function() {
+      var input = $(this).find('input');
+      $(this).find('button').click(function() {
+        var gen = capServer.restore(input.val());
+        gen.get(
+          function(newLaunch) { setLaunchCap(newLaunch); },
+          function(err) { alert("Generation failed."); });
+      });
+    });
+  
+    window.addEventListener('storage', function(ev) {
+      if(ev.storageArea === localStorage && ev.key == 'belay') {
+        belayData = capServer.dataPostProcess(localStorage['belay']);
+        resetUI();
+      }
+    });
+    
+    resetUI();
+    window.belaytest.ready = true;
+  });
+});
+

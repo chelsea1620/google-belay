8d07541a5af3ffcd0ea618c28ec3147f8b4e0442
Mark Lentczner <mzero@google.com>
2011-11-23 14:02:08 -0800

    refactored your-sites.js via requirejs

--- station/utils.js
+++ station/utils.js
@@ -12,25 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-(function(){
-  if (window.belay) return;
-      // There is belay system already in this browser, we're done
+define([], function() {
+  
+  function detachProto(elem) {
+    var proto = elem.eq(0).detach();
+    proto.removeClass('proto');
+    return proto;
+  }
 
-  // set-up and load the belay system from the well known service
-  var BELAY_BASE = '##BASE##';
-  var BELAY_PORT_SCRIPT = BELAY_BASE + "/belay-port.js";
-
-  window.belay = {
-    DEBUG: false,
-    BASE: BELAY_BASE,
-    PORT_SCRIPT: BELAY_PORT_SCRIPT,
-    
-    wasReady: false,
-    portReady: function() { window.belay.wasReady = true; }
-  };
-
-  var script = document.createElement('script');
-  script.setAttribute('src', BELAY_PORT_SCRIPT);
-  document.head.appendChild(script);
-})();
+  return {
+    detachProto: detachProto,
+  }
+});
 
